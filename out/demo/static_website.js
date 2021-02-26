33f8737940d3cc0a4b2bda916010869b62a24be6
Jacob Chvatal <jake@isnt.online>
2021-02-25 21:42:46 -0800

    Fix arvelie time

--- out/demo/static_website.js
+++ out/demo/static_website.js
@@ -1,25 +1,57 @@
 // Compiled by ClojureScript 1.10.597 {}
 goog.provide('demo.static_website');
 goog.require('cljs.core');
-demo.static_website.updateTime = (function demo$static_website$updateTime(k){
-if((k < (10))){
-return ["0",cljs.core.str.cljs$core$IFn$_invoke$arity$1(k)].join('');
+demo.static_website.foldr = (function demo$static_website$foldr(fnc,base,lst){
+if(cljs.core.empty_QMARK_.call(null,lst)){
+return base;
 } else {
-return k;
+return fnc.call(null,cljs.core.first.call(null,lst),demo.static_website.foldr.call(null,cljs.core.rest.call(null,lst)));
 }
 });
-demo.static_website.getCurrentTime = (function demo$static_website$getCurrentTime(){
+demo.static_website.foldl = (function demo$static_website$foldl(fnc,base,lst){
+if(cljs.core.empty_QMARK_.call(null,lst)){
+return base;
+} else {
+return demo.static_website.foldl.call(null,fnc,fnc.call(null,cljs.core.first.call(null,lst),base),cljs.core.rest.call(null,lst));
+}
+});
+demo.static_website.padTime = (function demo$static_website$padTime(pad,k){
+return demo.static_website.foldl.call(null,cljs.core.str,k,cljs.core.repeat.call(null,(pad - cljs.core.str.cljs$core$IFn$_invoke$arity$1(k).length),(0)));
+});
+demo.static_website.getTimes = (function demo$static_website$getTimes(){
 var date = (new Date());
-var hrs = demo.static_website.updateTime.call(null,date.getHours());
-var min = demo.static_website.updateTime.call(null,date.getMinutes());
-var sec = demo.static_website.updateTime.call(null,date.getSeconds());
+var hrs = date.getHours();
+var min = date.getMinutes();
+var sec = date.getSeconds();
+return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [hrs,min,sec], null);
+});
+demo.static_website.getStandardTime = (function demo$static_website$getStandardTime(){
+var date = (new Date());
+var hrs = demo.static_website.padTime.call(null,(2),date.getHours());
+var min = demo.static_website.padTime.call(null,(2),date.getMinutes());
+var sec = demo.static_website.padTime.call(null,(2),date.getSeconds());
 return [cljs.core.str.cljs$core$IFn$_invoke$arity$1(hrs),":",cljs.core.str.cljs$core$IFn$_invoke$arity$1(min),":",cljs.core.str.cljs$core$IFn$_invoke$arity$1(sec)].join('');
 });
-demo.static_website.setCurrentTime = (function demo$static_website$setCurrentTime(){
-(document.getElementById("clock").innerText = demo.static_website.getCurrentTime.call(null));
-
-return setTimeout(demo.static_website.setCurrentTime,(1000));
+demo.static_website.getSeconds = (function demo$static_website$getSeconds(){
+var date = (new Date());
+var hrs = date.getHours();
+var min = date.getMinutes();
+var sec = date.getSeconds();
+return (sec + ((60) * (min + (hrs * (60)))));
 });
-demo.static_website.setCurrentTime.call(null);
+demo.static_website.getNeralieTime = (function demo$static_website$getNeralieTime(){
+var pulses = (demo.static_website.getSeconds.call(null) / 86.4);
+var beat = Math.floor(pulses);
+var pulse = cljs.core.str.cljs$core$IFn$_invoke$arity$1(Math.floor((pulses * (1000)))).substring((3));
+return [cljs.core.str.cljs$core$IFn$_invoke$arity$1(demo.static_website.padTime.call(null,(3),beat)),":",cljs.core.str.cljs$core$IFn$_invoke$arity$1(demo.static_website.padTime.call(null,(3),pulse))].join('');
+});
+demo.static_website.setCurrentTime = (function demo$static_website$setCurrentTime(elementId,getTimeFunc){
+(document.getElementById(elementId).innerText = getTimeFunc.call(null));
+
+return setTimeout((function (){
+return demo.static_website.setCurrentTime.call(null,elementId,getTimeFunc);
+}),(0));
+});
+demo.static_website.setCurrentTime.call(null,"clock",demo.static_website.getNeralieTime);
 
 //# sourceMappingURL=static_website.js.map
--- src/demo/static_website.cljs
+++ src/demo/static_website.cljs
@@ -1,21 +1,57 @@
 (ns demo.static-website)
 
+(defn foldr [fnc base lst]
+  (if (empty? lst)
+    base
+    (fnc (first lst) (foldr (rest lst)))))
+
+(defn foldl [fnc base lst]
+  (if (empty? lst)
+    base
+    (foldl fnc (fnc (first lst) base) (rest lst))))
+
 ; truncate the time by prefixing it with a 0 as needed
-(defn updateTime [k]
-  (if (< k 10) (str "0" k) k))
+(defn padTime [pad k]
+  ;; (js/console.log (str (repeat (- pad (.-length (str k))) 0)))
+  (foldl str k (repeat (- pad (.-length (str k))) 0)))
+
+; get times from the javascript date object
+(defn getTimes []
+  (let [date (js/Date.)]
+    (let [hrs (.getHours date)
+          min (.getMinutes date)
+          sec (.getSeconds date)]
+      [hrs, min, sec])))
 
 ; get the current time
 (defn getStandardTime []
   (let [date (js/Date.)]
-    (let [hrs (updateTime (.getHours date))
-          min (updateTime (.getMinutes date))
-          sec (updateTime (.getSeconds date))]
+    (let [hrs (padTime 2 (.getHours date))
+          min (padTime 2 (.getMinutes date))
+          sec (padTime 2 (.getSeconds date))]
     (str hrs ":" min ":" sec))))
+
+;; (defn remainder [x y] (* (Math/floor (/ y x)) y))
+
+;; get the number of seconds that have passed
+(defn getSeconds []
+  (let [date (js/Date.)]
+    (let [hrs (.getHours date)
+          min (.getMinutes date)
+          sec (.getSeconds date)]
+      (+ sec (* 60 (+ min (* hrs 60)))))))
+
+; get the neralie time
+(defn getNeralieTime []
+  (let [pulses (/ (getSeconds) 86.4)]
+    (let [beat (Math/floor pulses)
+          pulse (.substring (str (Math/floor (* pulses 1000))) 3)]
+      (str (padTime 3 beat) ":" (padTime 3 pulse)))))
 
 ; update the website with the current time
 (defn setCurrentTime [elementId getTimeFunc]
     (set! (.-innerText (.getElementById js/document elementId)) (getTimeFunc))
-    (js/setTimeout (fn [] (setCurrentTime elementId getTimeFunc)) 1000))
+    (js/setTimeout (fn [] (setCurrentTime elementId getTimeFunc)) 0))
 
 ;; start the code
-(setCurrentTime "clock" getStandardTime)
+(setCurrentTime "clock" getNeralieTime)
