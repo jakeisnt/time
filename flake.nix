85134042e78d3197cfdb374cfc0e640ad04bbad2
Jacob Chvatal <jake@isnt.online>
2021-02-25 19:49:51 -0800

    configure clojure as default package?

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "clock web application";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        inherit (lib) attrValues;
+        lib = nixpkgs.lib;
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        defaultPackage = pkgs.clojure;
+        # with pkgs;
+        #   (stdenv.mkDerivation {
+        #     name = "time";
+        #     version = "0.0.1";
+        #     src = self;
+        #     buildInputs = [ clojure ];
+        #     installPhase = ''
+        #       clj -m cljs.main -d "out" -c demo.static-website -r
+        #     '';
+        #   });
+      });
+}
---
+++ out/cljs/user/static_websiteEC20131.cljs
@@ -0,0 +1,22 @@
+(ns demo.static-website)
+(require '[nano-id.core :refer [nano-id]])
+
+; truncate the time by prefixing it with a 0 as needed
+(defn updateTime [k]
+  (if (< k 10) (str "0" k) k))
+
+; get the current time
+(defn getStandardTime []
+  (let [date (js/Date.)]
+    (let [hrs (updateTime (.getHours date))
+          min (updateTime (.getMinutes date))
+          sec (updateTime (.getSeconds date))]
+    (str hrs ":" min ":" sec))))
+
+; update the website with the current time
+(defn setCurrentTime [elementId getTimeFunc]
+    (set! (.-innerText (.getElementById js/document elementId)) (getTimeFunc))
+    (js/setTimeout (fn [] (setCurrentTime elementId getTimeFunc)) 1000))
+
+;; start the code
+(setCurrentTime "clock" getStandardTime)
