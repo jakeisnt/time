8cb6b067987a2435e43609943b99a6971f9eb044
jakeisnt <jakechvatal@gmail.com>
2021-02-27 13:35:26 -0800

    Show time relative to age (#9)
    * add optional arg to getTimes
    
    * Single source for getting defined times
    
    * get age working with fixed time
    
    * start application more easily
    
    * Fix date hashing fetch
    
    * set everything with a single animation func
    
    * Minor cleanup from errs

--- out/time/main.cljs
+++ out/time/main.cljs
@@ -3,7 +3,7 @@
 (defn foldr [fnc base lst]
   (if (empty? lst)
     base
-    (fnc (first lst) (foldr (rest lst)))))
+    (fnc (first lst) (foldr fnc base (rest lst)))))
 
 (defn foldl [fnc base lst]
   (if (empty? lst)
@@ -12,72 +12,80 @@
 
 ; truncate the time by prefixing it with a 0 as needed
 (defn padTime [pad k]
-  ;; (js/console.log (str (repeat (- pad (.-length (str k))) 0)))
   (foldl str k (repeat (- pad (.-length (str k))) 0)))
 
 ; get times from the javascript date object
-;; (defn getTimes []
-;;   (let [date (js/Date.)]
-;;     (let [hrs (.getHours date)
-;;           min (.getMinutes date)
-;;           sec (.getSeconds date)]
-;;       {:hours hrs :minutes min :seconds sec})))
+(defn getTimes [& [d]]
+  (let [date (if d d (js/Date.))
+        hrs (.getHours date)
+        min (.getMinutes date)
+        sec (.getSeconds date)]
+    {:hrs hrs :mins min :secs sec}))
 
 ; get the current time
 (defn getStandardTime []
-  (let [date (js/Date.)]
-    (let [hrs (padTime 2 (.getHours date))
-          min (padTime 2 (.getMinutes date))
-          sec (padTime 2 (.getSeconds date))]
-    (str hrs ":" min ":" sec))))
+  (let [date (getTimes)
+        hrs (padTime 2 (:hrs date))
+        min (padTime 2 (:mins date))
+        sec (padTime 2 (:secs date))]
+    (str hrs ":" min ":" sec)))
 
 ;; get the number of seconds that have passed
 (defn getSeconds []
-  (let [date (js/Date.)]
-    (let [hrs (.getHours date)
-          min (.getMinutes date)
-          sec (.getSeconds date)]
-      (+ sec (* 60 (+ min (* hrs 60)))))))
+  (let [date (getTimes)
+        hrs (:hours date)
+        min (:mins date)
+        sec (:secs date)]
+    (+ sec (* 60 (+ min (* hrs 60))))))
 
 ; get the neralie time
 (defn getNeralieTime []
-  (let [pulses (/ (getSeconds) 86.4)]
-    (let [beat (Math/floor pulses)
-          pulse (.substring (str (Math/floor (* pulses 1000))) 3)]
-      (str (padTime 3 beat) ":" (padTime 3 pulse)))))
+  (let [pulses (/ (getSeconds) 86.4)
+        beat (Math/floor pulses)
+        pulse (.substring (str (Math/floor (* pulses 1000))) 3)]
+    (str (padTime 3 beat) ":" (padTime 3 pulse))))
 
 ; update the website with the current time
 (defn setCurrentTime [elementId getTimeFunc]
-    (set! (.-innerText (.getElementById js/document elementId)) (getTimeFunc))
-    (js/setTimeout (fn [] (setCurrentTime elementId getTimeFunc)) 0))
-
-;; list of clocks to use and their names
-(def CLOCKLIST (list (list "numclock" getStandardTime)
-                     (list "numneralie" getNeralieTime)))
-
+  (set! (.-innerText (.getElementById js/document elementId)) (getTimeFunc)))
 
 ;; from https://medium.com/@abhi95.saxena/make-an-analog-clock-using-javascript-7c07580ea91b
 (defn runClock []
   (let [hourhand (.querySelector js/document "#hour")
-       minutehand (.querySelector js/document "#minute")
-       secondhand (.querySelector js/document "#second")]
-  (let [date (js/Date.)]
-    (let [hrs (.getHours date)
-          min (.getMinutes date)
-          sec (.getSeconds date)]
-        (let [hrPos (+ (/ (* hrs 360) 12) (/ (/ (* min 360) 60) 12))
-              minPos (+ (/ (* min 60) 60) (/ (/ (* sec 60) 60) 60))
-              secPos (/ (* sec 360) 60)]
-          (set! (.-transform (.-style hourhand)) (str "rotate(" hrPos "deg)"))
-          (set! (.-transform (.-style minutehand)) (str "rotate(" minPos "deg)"))
-          (set! (.-transform (.-style secondhand)) (str "rotate(" secPos "deg)"))
-          (js/setTimeout runClock 1000))))))
+        minutehand (.querySelector js/document "#minute")
+        secondhand (.querySelector js/document "#second")
+        date (getTimes)
+        hrs (:hrs date)
+        min (:mins date)
+        sec (:secs date)
+        hrPos (+ (/ (* hrs 360) 12) (/ (/ (* min 360) 60) 12))
+        minPos (+ (/ (* min 60) 60) (/ (/ (* sec 60) 60) 60))
+        secPos (/ (* sec 360) 60)]
+    (set! (.-transform (.-style hourhand)) (str "rotate(" hrPos "deg)"))
+    (set! (.-transform (.-style minutehand)) (str "rotate(" minPos "deg)"))
+    (set! (.-transform (.-style secondhand)) (str "rotate(" secPos "deg)"))))
 
-;; given an element of the clocklist, make
-(defn makeElement [ls]
-  (let [elemId (first ls)
-        elemTimeFunc (nth ls 1)]
-    (setCurrentTime elemId elemTimeFunc)))
+;; show age starting at the provided date
+;; inspiration: https://github.com/neauoire/age/blob/master/index.html
+(defn showAge [date]
+  (set! (.-innerText (.getElementById js/document "numage"))
+        (.toFixed (/ (- (js/Date.) (js/Date. date)) 31557600000) 9)))
 
-(run! makeElement CLOCKLIST)
-(runClock)
+;; start the show age function
+(defn start []
+  ;; if nothing is provided for the hash, set it to my birthday!
+  (if (= "" (.-hash (.-location js/window)))
+    (set! (.-hash (.-location js/window)) "1999-11-05") nil)
+
+  (let [datehash (.replace (.-hash (.-location js/window)) "#" "")]
+    (js/setTimeout
+     (fn []
+       (showAge datehash)
+       (runClock)
+       (setCurrentTime "numclock" getStandardTime)
+       (setCurrentTime "numneralie" getNeralieTime)
+       (js/requestAnimationFrame start))
+     (/ 1000 30))))
+
+;; start the application
+(start)
--- out/time/main.js
+++ out/time/main.js
@@ -5,7 +5,7 @@ time.main.foldr = (function time$main$foldr(fnc,base,lst){
 if(cljs.core.empty_QMARK_.call(null,lst)){
 return base;
 } else {
-return fnc.call(null,cljs.core.first.call(null,lst),time.main.foldr.call(null,cljs.core.rest.call(null,lst)));
+return fnc.call(null,cljs.core.first.call(null,lst),time.main.foldr.call(null,fnc,base,cljs.core.rest.call(null,lst)));
 }
 });
 time.main.foldl = (function time$main$foldl(fnc,base,lst){
@@ -18,18 +18,56 @@ return time.main.foldl.call(null,fnc,fnc.call(null,cljs.core.first.call(null,lst
 time.main.padTime = (function time$main$padTime(pad,k){
 return time.main.foldl.call(null,cljs.core.str,k,cljs.core.repeat.call(null,(pad - cljs.core.str.cljs$core$IFn$_invoke$arity$1(k).length),(0)));
 });
-time.main.getStandardTime = (function time$main$getStandardTime(){
-var date = (new Date());
-var hrs = time.main.padTime.call(null,(2),date.getHours());
-var min = time.main.padTime.call(null,(2),date.getMinutes());
-var sec = time.main.padTime.call(null,(2),date.getSeconds());
-return [cljs.core.str.cljs$core$IFn$_invoke$arity$1(hrs),":",cljs.core.str.cljs$core$IFn$_invoke$arity$1(min),":",cljs.core.str.cljs$core$IFn$_invoke$arity$1(sec)].join('');
+time.main.getTimes = (function time$main$getTimes(var_args){
+var args__4795__auto__ = [];
+var len__4789__auto___531 = arguments.length;
+var i__4790__auto___532 = (0);
+while(true){
+if((i__4790__auto___532 < len__4789__auto___531)){
+args__4795__auto__.push((arguments[i__4790__auto___532]));
+
+var G__533 = (i__4790__auto___532 + (1));
+i__4790__auto___532 = G__533;
+continue;
+} else {
+}
+break;
+}
+
+var argseq__4796__auto__ = ((((0) < args__4795__auto__.length))?(new cljs.core.IndexedSeq(args__4795__auto__.slice((0)),(0),null)):null);
+return time.main.getTimes.cljs$core$IFn$_invoke$arity$variadic(argseq__4796__auto__);
 });
-time.main.getSeconds = (function time$main$getSeconds(){
-var date = (new Date());
+
+(time.main.getTimes.cljs$core$IFn$_invoke$arity$variadic = (function (p__527){
+var vec__528 = p__527;
+var d = cljs.core.nth.call(null,vec__528,(0),null);
+var date = (cljs.core.truth_(d)?d:(new Date()));
 var hrs = date.getHours();
 var min = date.getMinutes();
 var sec = date.getSeconds();
+return new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"hrs","hrs",-1479098314),hrs,new cljs.core.Keyword(null,"mins","mins",467369676),min,new cljs.core.Keyword(null,"secs","secs",1532330091),sec], null);
+}));
+
+(time.main.getTimes.cljs$lang$maxFixedArity = (0));
+
+/** @this {Function} */
+(time.main.getTimes.cljs$lang$applyTo = (function (seq526){
+var self__4777__auto__ = this;
+return self__4777__auto__.cljs$core$IFn$_invoke$arity$variadic(cljs.core.seq.call(null,seq526));
+}));
+
+time.main.getStandardTime = (function time$main$getStandardTime(){
+var date = time.main.getTimes.call(null);
+var hrs = time.main.padTime.call(null,(2),new cljs.core.Keyword(null,"hrs","hrs",-1479098314).cljs$core$IFn$_invoke$arity$1(date));
+var min = time.main.padTime.call(null,(2),new cljs.core.Keyword(null,"mins","mins",467369676).cljs$core$IFn$_invoke$arity$1(date));
+var sec = time.main.padTime.call(null,(2),new cljs.core.Keyword(null,"secs","secs",1532330091).cljs$core$IFn$_invoke$arity$1(date));
+return [cljs.core.str.cljs$core$IFn$_invoke$arity$1(hrs),":",cljs.core.str.cljs$core$IFn$_invoke$arity$1(min),":",cljs.core.str.cljs$core$IFn$_invoke$arity$1(sec)].join('');
+});
+time.main.getSeconds = (function time$main$getSeconds(){
+var date = time.main.getTimes.call(null);
+var hrs = new cljs.core.Keyword(null,"hours","hours",58380855).cljs$core$IFn$_invoke$arity$1(date);
+var min = new cljs.core.Keyword(null,"mins","mins",467369676).cljs$core$IFn$_invoke$arity$1(date);
+var sec = new cljs.core.Keyword(null,"secs","secs",1532330091).cljs$core$IFn$_invoke$arity$1(date);
 return (sec + ((60) * (min + (hrs * (60)))));
 });
 time.main.getNeralieTime = (function time$main$getNeralieTime(){
@@ -39,21 +77,16 @@ var pulse = cljs.core.str.cljs$core$IFn$_invoke$arity$1(Math.floor((pulses * (10
 return [cljs.core.str.cljs$core$IFn$_invoke$arity$1(time.main.padTime.call(null,(3),beat)),":",cljs.core.str.cljs$core$IFn$_invoke$arity$1(time.main.padTime.call(null,(3),pulse))].join('');
 });
 time.main.setCurrentTime = (function time$main$setCurrentTime(elementId,getTimeFunc){
-(document.getElementById(elementId).innerText = getTimeFunc.call(null));
-
-return setTimeout((function (){
-return time.main.setCurrentTime.call(null,elementId,getTimeFunc);
-}),(0));
+return (document.getElementById(elementId).innerText = getTimeFunc.call(null));
 });
-time.main.CLOCKLIST = (new cljs.core.List(null,(new cljs.core.List(null,"numclock",(new cljs.core.List(null,time.main.getStandardTime,null,(1),null)),(2),null)),(new cljs.core.List(null,(new cljs.core.List(null,"numneralie",(new cljs.core.List(null,time.main.getNeralieTime,null,(1),null)),(2),null)),null,(1),null)),(2),null));
 time.main.runClock = (function time$main$runClock(){
 var hourhand = document.querySelector("#hour");
 var minutehand = document.querySelector("#minute");
 var secondhand = document.querySelector("#second");
-var date = (new Date());
-var hrs = date.getHours();
-var min = date.getMinutes();
-var sec = date.getSeconds();
+var date = time.main.getTimes.call(null);
+var hrs = new cljs.core.Keyword(null,"hrs","hrs",-1479098314).cljs$core$IFn$_invoke$arity$1(date);
+var min = new cljs.core.Keyword(null,"mins","mins",467369676).cljs$core$IFn$_invoke$arity$1(date);
+var sec = new cljs.core.Keyword(null,"secs","secs",1532330091).cljs$core$IFn$_invoke$arity$1(date);
 var hrPos = (((hrs * (360)) / (12)) + (((min * (360)) / (60)) / (12)));
 var minPos = (((min * (60)) / (60)) + (((sec * (60)) / (60)) / (60)));
 var secPos = ((sec * (360)) / (60));
@@ -61,16 +94,30 @@ var secPos = ((sec * (360)) / (60));
 
 (minutehand.style.transform = ["rotate(",cljs.core.str.cljs$core$IFn$_invoke$arity$1(minPos),"deg)"].join(''));
 
-(secondhand.style.transform = ["rotate(",cljs.core.str.cljs$core$IFn$_invoke$arity$1(secPos),"deg)"].join(''));
+return (secondhand.style.transform = ["rotate(",cljs.core.str.cljs$core$IFn$_invoke$arity$1(secPos),"deg)"].join(''));
+});
+time.main.showAge = (function time$main$showAge(date){
+return (document.getElementById("numage").innerText = (((new Date()) - (new Date(date))) / (31557600000)).toFixed((9)));
+});
+time.main.start = (function time$main$start(){
+if(cljs.core._EQ_.call(null,"",window.location.hash)){
+(window.location.hash = "1999-11-05");
+} else {
+}
 
-return setTimeout(time.main.runClock,(1000));
-});
-time.main.makeElement = (function time$main$makeElement(ls){
-var elemId = cljs.core.first.call(null,ls);
-var elemTimeFunc = cljs.core.nth.call(null,ls,(1));
-return time.main.setCurrentTime.call(null,elemId,elemTimeFunc);
-});
-cljs.core.run_BANG_.call(null,time.main.makeElement,time.main.CLOCKLIST);
+var datehash = window.location.hash.replace("#","");
+return setTimeout((function (){
+time.main.showAge.call(null,datehash);
+
 time.main.runClock.call(null);
+
+time.main.setCurrentTime.call(null,"numclock",time.main.getStandardTime);
+
+time.main.setCurrentTime.call(null,"numneralie",time.main.getNeralieTime);
+
+return requestAnimationFrame(time.main.start);
+}),((1000) / (30)));
+});
+time.main.start.call(null);
 
 //# sourceMappingURL=main.js.map
